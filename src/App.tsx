36e1dd9d0977e0b5c96b4a1dc75fe274e2789b2c
valentin9 <valentinderksen@gmail.com>
2020-06-21 00:48:25 +0200

    removed demo menu items

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,8 @@ import {
 } from 'react-router-dom';
 
 import { Layout, Menu } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import ConfigList from './components/ConfigList';
 
-const { SubMenu } = Menu;
 const { Header, Sider } = Layout;
 
 const API_CONFIG_LIST = 'http://localhost:3100/nginx-configs';
@@ -28,12 +26,9 @@ function App(props: any) {
   return (
     <Layout>
       <Header className="header">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
+        <h3 style={{color: 'white'}}>
+          Nginx Config Manager with co-op
+        </h3>
       </Header>
       <Router>
         <Layout>
@@ -44,24 +39,12 @@ function App(props: any) {
               defaultOpenKeys={['sub1']}
               style={{ height: '100%', borderRight: 0 }}
             >
-              <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
-              </SubMenu>
+              <Menu.Item key="1">
+                <Link to={'/'}>Home</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to={'/configs'}>My configs</Link>
+              </Menu.Item>
             </Menu>
           </Sider>
           <Layout style={{ padding: '0 24px 24px' }}>
