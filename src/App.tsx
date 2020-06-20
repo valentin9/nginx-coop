9850b30c0981fb00281ab00795271cd1d4537e96
valentin9 <valentinderksen@gmail.com>
2020-06-21 00:17:09 +0200

    updated react app to use hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'antd/dist/antd.css'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -27,6 +27,72 @@ const columns = [
   },
 ];
 
+
+function Appy(props: any) {
+  const [configList, setConfigList] = useState<undefined|Object[]>(undefined);
+
+  useEffect(() => {
+    getList((data: Object[]) => {
+      setConfigList(data);
+    });
+  }, []);
+
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Router>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                <Menu.Item key="1">option1</Menu.Item>
+                <Menu.Item key="2">option2</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+                <Menu.Item key="7">option7</Menu.Item>
+                <Menu.Item key="8">option8</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                <Menu.Item key="9">option9</Menu.Item>
+                <Menu.Item key="10">option10</Menu.Item>
+                <Menu.Item key="11">option11</Menu.Item>
+                <Menu.Item key="12">option12</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Switch>
+              <Route path="/configs">
+                <ConfigList configList={configList} />
+              </Route>
+              <Route path="/configs">
+                <ConfigList />
+              </Route>
+            </Switch>
+
+          </Layout>
+        </Layout>
+      </Router>
+    </Layout>
+  );
+}
+
 function getList(callback: Function): void
 {
   fetch(API_CONFIG_LIST, { headers: { "Content-Type": "application/json; charset=utf-8" }})
@@ -37,76 +103,6 @@ function getList(callback: Function): void
     .catch(err => {
         console.log(err);
     });
-}
-
-class App extends React.Component<{}, { configList?: any[] }> {
-  constructor(props: Object) {
-    super(props);
-    this.state = { configList: undefined }
-  }
-
-  componentDidMount() {
-    getList((response: any) => {
-      this.setState({configList: response});
-    });
-  }
-
-  render() {
-    return (
-      <Layout>
-        <Header className="header">
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Router>
-          <Layout>
-            <Sider width={200} className="site-layout-background">
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
-              >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
-              <Switch>
-                <Route path="/configs">
-                  <ConfigList />
-                </Route>
-                <Route path="/configs">
-                  <ConfigList />
-                </Route>
-              </Switch>
-
-            </Layout>
-          </Layout>
-        </Router>
-      </Layout>
-    );
-  }
 }
 
 function ConfigList(props: { configList?: any[] }) {
@@ -125,7 +121,7 @@ function ConfigList(props: { configList?: any[] }) {
           minHeight: 280,
         }}
         >
-        <Table dataSource={props.configList} columns={columns} />;
+        <Table dataSource={props.configList} columns={columns} />
       </Content>
     </div>
   );
@@ -153,4 +149,4 @@ function ConfigDetail({config}: { config?: Object }) {
   );
 }
 
-export default App;
+export default Appy;
